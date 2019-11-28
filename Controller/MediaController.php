8352bc16f61a7a3dcff06dccd0070ad5d2346875
hbernaciak <hubert.bernaciak@appverk.com>
2019-11-28 17:05:01 +0100

    fixed annotation

--- Controller/MediaController.php
+++ Controller/MediaController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/media")
